c9ee6d2aa595d0b6d2693d091998d6f1c94904ba
Jordan Janszen <jordanjanszen@hotmail.com>
2024-04-12 09:23:04 +1000

    Lab Part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+    titles = models.CharField(max_length=100)
+    microchipnr = models.IntegerField()
+    handler = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
