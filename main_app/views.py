2a5783a5efbc0b28ffabc17c833265483f94ef65
Jordan Janszen <jordanjanszen@hotmail.com>
2024-04-12 10:00:23 +1000

    Lab Part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Create your views here.
@@ -21,3 +21,16 @@ def dogs_detail(request, dog_id):
     return render(request, 'dogs/detail.html', {
         'dog': dog
     })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/{dog_id}'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = ['name', 'description', 'age', 'titles', 'handler']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs'
